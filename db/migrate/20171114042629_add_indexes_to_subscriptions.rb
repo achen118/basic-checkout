43540c0956072590689ea0e66744f67a45895958
Alice Chen <alicechen118@gmail.com>
2017-11-13 20:30:19 -0800

    Add indexes to subscriptions table

--- db/migrate/20171114042629_add_indexes_to_subscriptions.rb
+++ db/migrate/20171114042629_add_indexes_to_subscriptions.rb
@@ -2,6 +2,6 @@ class AddIndexesToSubscriptions < ActiveRecord::Migration[5.1]
   def change
     add_index :subscriptions, :user_id
     add_index :subscriptions, :membership_plan_id
-    add_index :subscriptions, [:user_id, :membership_plan_id], uniqueness: true
+    add_index :subscriptions, [:user_id, :membership_plan_id], unique: true
   end
 end
