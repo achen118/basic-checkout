f9c0a69a39978b30156a831e4c537f3e86c9f234
Alice Chen <alicechen118@gmail.com>
2017-11-14 20:44:11 -0800

    Refactor membership plan react components

--- client/src/components/subscriptions/subscriptionPage.js
+++ client/src/components/subscriptions/subscriptionPage.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import MembershipPlansContainer from '../membershipPlans/membershipPlansContainer';
+import MembershipPlan from '../membershipPlans/membershipPlan';
 import '../../styles/subscriptions.css';
 
-export default class AuthForm extends Component {
+export default class SubscriptionPage extends Component {
+    componentDidMount() {
+        this.props.fetchAllMembershipPlans();
+    }
+
     constructor(props) {
         super(props);
+        this.state = {
+        };
     }
 
     render() {
-        console.log(this.props);
-        console.log(localStorage);
+        const { membershipPlans } = this.props;
         return (
             <div className="subscription-page-container">
-                <MembershipPlansContainer />
+                <h2 className="subscription-page-title">
+                    My Subscription Page
+                </h2>
+                {
+                    membershipPlans.map((membershipPlan, idx) => (
+                        <section>
+                            <MembershipPlan membershipPlan={ membershipPlan } key={ idx } />
+                            <label> # of Guests 
+                                <input
+                                    defaultValue="0"
+                                    id={ membershipPlan.level } />
+                            </label>
+                            <button>
+                                Subscribe Now
+                            </button>
+                        </section>
+                    ))
+                }
             </div>
         );
     }
--- client/src/components/subscriptions/subscriptionPageContainer.js
+++ client/src/components/subscriptions/subscriptionPageContainer.js
@@ -1,15 +1,18 @@
 import { connect } from 'react-redux';
 import SubscriptionPage from './subscriptionPage';
+import { fetchAllMembershipPlans } from '../../actions/membershipPlansActions';
 
 const mapStateToProps = state => {
     return {
-        currentUser: state.currentUser
+        currentUser: state.currentUser,
+        membershipPlans: state.membershipPlans,
+        subscriptions: state.subscriptions
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        
+        fetchAllMembershipPlans: () => dispatch(fetchAllMembershipPlans())
     };
 };
 
