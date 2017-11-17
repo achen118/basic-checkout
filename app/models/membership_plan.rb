b2dc1557fe07ab3d10225592fc3163e5be9a3449
Alice Chen <alicechen118@gmail.com>
2017-11-16 21:35:38 -0800

    Fix model validations

--- app/models/membership_plan.rb
+++ app/models/membership_plan.rb
@@ -1,4 +1,6 @@
 class MembershipPlan < ApplicationRecord
+    validates :level, :description, :cost, :guest_cost, presence: true
+
     has_many :subscriptions
     has_many :users, through: :subscriptions
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,7 +1,15 @@
 class Subscription < ApplicationRecord
-    validates :user, :membership_plan, presence: true
+    validates :user, :membership_plan, :cost, :guests, :stripe_token, presence: true
     validates :user, uniqueness: { scope: :membership_plan }
 
     belongs_to :user
     belongs_to :membership_plan
+
+    def process_payment
+    customer = Stripe::Customer.create(email: email, card: stripe_token)
+    Stripe::Charge.create customer: customer.id,
+                          amount: course.price * 100,
+                          description: course.name,
+                          currency: 'usd'
+    end
 end
